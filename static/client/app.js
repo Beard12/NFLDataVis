7603c84e29d5dd1a0838d60dcb799f7672072563
Jacob Crescent <jacobcrescent@gmail.com>
2016-09-02 15:07:18 -0400

    added angular

---
+++ static/client/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('app', ['ngMaterial', 'ngRoute']);
+app.config(function($routeProvider){
+$routeProvider
+	.when('/', {
+			templateUrl: 'static/client/partials/home.html',
+			controller: 'homeController'
+	})
+	.when('/toppassers', {
+		templateUrl: 'static/client/partials/passing.html',
+		controller: 'homeController'
+	})
+	.when('/rushvspass', {
+		templateUrl: 'static/client/partials/rushvspass.html', 
+		controller: 'homeController'
+	})
+});
